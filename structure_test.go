15e315d338126a81919d59aab12e6d737ef49b90
Frederick Robinson <frederick.robinson@frrad.com>
2021-01-01 20:58:11 -0800

    more diff context

--- structure_test.go
+++ structure_test.go
@@ -48,12 +48,42 @@ func RoundtripBytes(t *testing.T, b []byte) {
 
 func CompareBytes(t *testing.T, a, b []byte) {
 	if len(a) != len(b) {
-		t.Fatalf("different lens %d != %d", len(a), len(b))
+		t.Errorf("different lens %d != %d", len(a), len(b))
 	}
 
-	for i := 0; i < len(a); i++ {
+	min := len(a)
+	if len(b) < min {
+		min = len(b)
+	}
+
+	brokenIx := -1
+
+	for i := 0; i < min; i++ {
 		if a[i] != b[i] {
-			t.Error(a[i], b[i])
+			t.Errorf("first mismatch at offset %d", i)
+			brokenIx = i
+			break
 		}
+	}
+
+	if brokenIx != -1 {
+
+		width := 25
+		from, to := brokenIx-width, brokenIx+width
+		if from < 0 {
+			from = 0
+		}
+		if to < 0 {
+			from = 0
+		}
+		if from > min {
+			from = min
+		}
+		if to > min {
+			from = min
+		}
+
+		t.Errorf("snippet\na=\n%s\nb=\n%s", string(a[from:to]), string(b[from:to]))
+
 	}
 }
